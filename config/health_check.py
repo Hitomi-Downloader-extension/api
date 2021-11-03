d0fb671040bc65b628107ca9c1f13de6272be2e4
Ryu JuHeon <saidbysolo@gmail.com>
2021-11-03 22:12:48 +0900

    chore(config): add sleep

--- config/health_check.py
+++ config/health_check.py
@@ -1,10 +1,12 @@
 import requests
+import time
 
-for _ in range(5):
+for _ in range(1, 6):
     try:
         r = requests.get("http://localhost:6009/ping")
         if r.status_code == 200:
             print("OK")
             break
+        time.sleep(_)
     except:
         pass
