b6f61dc1392b4c88ca79309b23b3bdae0b54f53d
Ryu JuHeon <saidbysolo@gmail.com>
2021-11-03 22:16:25 +0900

    chore(config): use while true

--- config/health_check.py
+++ config/health_check.py
@@ -1,12 +1,15 @@
 import requests
 import time
 
-for _ in range(1, 6):
+t = 0
+while True:
+    t += 1
     try:
         r = requests.get("http://localhost:6009/ping")
         if r.status_code == 200:
             print("OK")
             break
-        time.sleep(_)
+        print(f"No response. try again after {t} second")
+        time.sleep(t)
     except:
         pass
