7f758297f3d890c770466f9b007e7fc869542349
Ryu juheon <saidbysolo@gmail.com>
2021-11-04 14:34:52 +0900

    test(download): fix typo

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,6 +2,6 @@ def test_download(client):
     r = client.post("http://localhost:6009/download", json={"gal_num": "1570712"})
     assert r.json() == {"status":"ok"}
 
-det test_no_gal_num_download(client):
+def test_no_gal_num_download(client):
     r = client.post("http://localhost:6009/download", json={"test": "payload"})
     assert r.json() == {"error":"bad_request"}
