a55e24dafae7ef6bbdf47349960f24fa6ec34a6d
Ryu juheon <saidbysolo@gmail.com>
2021-11-04 13:20:01 +0900

    test(download): call func

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,3 @@
 def test_download(client):
     r = client.post("http://localhost:6009/download", json={"gal_num": "1570712"})
-    assert r.json == {"status":"ok"}
+    assert r.json() == {"status":"ok"}
