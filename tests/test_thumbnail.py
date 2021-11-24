9eed09498f0b14df154e6129338f81d2ad9862f6
Ryu juheon <saidbysolo@gmail.com>
2021-11-24 15:24:25 +0900

    test: not status

--- tests/test_thumbnail.py
+++ tests/test_thumbnail.py
@@ -3,5 +3,5 @@ from requests.sessions import Session
 
 def test_thumbnail(client: Session):
     r = client.get("http://localhost:6009/thumbnail")
-    assert r.json() == {"status": "not_found"}
+    assert r.json() == {"error": "not_found"}
     assert r.status_code == 404
